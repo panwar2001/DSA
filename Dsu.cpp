7b31dd801ed7b68a86ca2fac491cbde5906ff878
Ayush Panwar <75212147+PanwarAyush@users.noreply.github.com>
2022-09-07 08:32:57 +0530

    Update Dsu.cpp

--- Dsu.cpp
+++ Dsu.cpp
@@ -4,21 +4,24 @@ private:
 public:
   DSU(int n){ //O(N) time & space complextiy for initialization of size and nodes.
     weigh.resize(n,1);
+    parent.resize(n);
     for(int i=0;i<n;i++)
       parent[i]=i;
   } 
-  int find(int x){
+  inline int find(int x){
      if(parent[x]==x)
         return x;
       return parent[x]=find(parent[x]);
   }
-  void Union(int a,int b){   //O(k) time complexity, here k<4 
+inline bool Union(int a,int b){   //O(k) time complexity, here k<4 
      a=find(a);
      b=find(b);
-     if(a==b)return;
+     if(a==b) 
+       return false;
      if(weigh[a]>weigh[b])
       swap(a,b);
      parent[a]=b;
      weigh[b]+=weigh[a];
+     return true;
   }
 };
